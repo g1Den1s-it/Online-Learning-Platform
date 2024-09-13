82d22f7345794ce645986192b08d48558069e2b0
g1Den1s-it <g1den1splay@gmail.com>
2024-09-13 22:29:57 +0300

    Start working with the user

---
+++ authorization/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+# Create your models here.
+
+ROLE = (("Teacher", "Teacher"),
+        ("Student", "Student"),
+        ("Administrator", "Administrator"))
+
+
+class User(AbstractUser):
+    role = models.CharField(max_length=26, choices=ROLE, blank=False)
